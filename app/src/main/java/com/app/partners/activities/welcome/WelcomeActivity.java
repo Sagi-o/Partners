c6cf4612e139d793cf94b448318736154bd30500
Dvir <dvirhacohen@gmail.com>
2019-11-10 13:51:24 +0200

    registerUi complete

--- app/src/main/java/com/app/partners/activities/welcome/WelcomeActivity.java
+++ app/src/main/java/com/app/partners/activities/welcome/WelcomeActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.app.partners.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+     EditText email;
+     EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +22,10 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent i = new Intent(getBaseContext(), RegisterActivity.class);
         startActivity(i);
     }
+
+    public void login(View v){
+
+    }
+
+
 }
