730a1072dfd862f8e592589e3700a6031069847f
Dvir <dvirhacohen@gmail.com>
2019-11-20 13:42:11 +0200

    verifying login

--- app/src/main/java/com/app/partners/activities/welcome/WelcomeActivity.java
+++ app/src/main/java/com/app/partners/activities/welcome/WelcomeActivity.java
@@ -1,21 +1,43 @@
 package com.app.partners.activities.welcome;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.app.partners.R;
+import com.app.partners.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeActivity extends AppCompatActivity {
-     EditText email;
-     EditText password;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+
+    private String email_st, password_st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        mDatabase = FirebaseDatabase.getInstance();
     }
 
     public void navigateToRegister(View v) {
@@ -24,8 +46,62 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     public void login(View v){
+        Log.d("WelcomeActivity", "Login clicked");
+        email_st = email.getText().toString();
+        password_st = password.getText().toString();
+        mAuth.signInWithEmailAndPassword(email_st, password_st)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
 
+                        Log.d("WelcomeActivity", "User is: " + firebaseUser);
+                        String uid = firebaseUser.getUid();
+
+                        getUserFromDb(uid);
+
+                        // Check user type by fetching User object from db
+                        // Navigate user to suitable activity
+                        // Save user object on app
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("WelcomeActivity", "Login failed!" + e);
+
+                    }
+        });
     }
+
+    public void getUserFromDb(String uid) {
+        if (!uid.isEmpty()) {
+            ValueEventListener userListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    if (dataSnapshot.exists()) {
+                        User user = dataSnapshot.getValue(User.class);
+                        if (user.isLandLord()){
+                            // navigate to landlord Activity
+                        } else {
+                            // navigate to Renter Activity
+                        }
+                    }
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Getting Post failed, log a message
+                    //  Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                    // ...
+                }
+            };
+            mDatabase.getReference("users/" + uid).addValueEventListener(userListener);
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/app/partners/models/User.java
+++ app/src/main/java/com/app/partners/models/User.java
@@ -6,54 +6,45 @@ public class User {
     private String last_name;
     private String email;
     private String phone;
-    private String password;
+//    private String password;
+    private boolean isLandLord;
+    private boolean isSettle;
+
 
     public String getFirst_name() {
         return first_name;
     }
-
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
+    public String getLast_name() { return last_name; }
+    public void setLast_name(String last_name) { this.last_name = last_name; }
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    public void setEmail(String email) { this.email = email; }
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
+//    public String getPassword() {
+//        return password;
+//    }
+//    public void setPassword(String password) { this.password = password; }
+    public boolean isLandLord() { return isLandLord; }
+    public void setLandLord(boolean landLord){ this.isLandLord = landLord; }
+    public boolean isSettle() { return isSettle; }
+    public void setSettle(boolean settle) { isSettle = settle; }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User(String first_name, String last_name, String email, String phone, String password ){
+    public User(String first_name, String last_name, String email, String phone, boolean isLandLord){
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
         this.phone = phone;
-        this.password = password;
+//        this.password = password;
+        this.isLandLord = isLandLord;
     }
 }
 
