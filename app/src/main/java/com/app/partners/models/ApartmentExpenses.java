a79c5bc4b00e99b50d258443eebe03597001ceef
Sagi Oshri <sagioshri@gmail.com>
2020-01-19 20:14:34 +0200

    stats

---
+++ app/src/main/java/com/app/partners/models/ApartmentExpenses.java
@@ -0,0 +1,10 @@
+package com.app.partners.models;
+
+import java.util.HashMap;
+
+public class ApartmentExpenses {
+    public int sum = 0;
+    // field "expenses" is in db only
+
+    public ApartmentExpenses() { }
+}
